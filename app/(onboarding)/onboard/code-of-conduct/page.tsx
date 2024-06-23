678b465afe4899e01d7ee6b7d7431a21bc85db9d
mentorscx <mentorscx@gmail.com>
2024-06-23 13:28:49 -0400

    added code of conduct page

--- app/(onboarding)/onboard/code-of-conduct/page.tsx
+++ app/(onboarding)/onboard/code-of-conduct/page.tsx
@@ -1,245 +1,82 @@
 "use client";
 import React from "react";
-import { Label } from "@/components/ui/label";
-import { Switch } from "@/components/ui/switch";
+
 import { CheckIcon, HeartHandshake, Loader2Icon } from "lucide-react";
 import Link from "next/link";
 
 import { Button } from "@/components/ui/button";
 
 import { useRouter } from "next/navigation";
-import { delay } from "@/lib/utils";
+
+const ConductItem = ({ text }: { text: string }) => (
+  <li className="flex items-center gap-4">
+    <CheckIcon className="h-5 w-5 shrink-0 text-primary-500" />
+    <span> {text}</span>
+  </li>
+);
 
 const Onboard5Page = () => {
   const router = useRouter();
-  const [role, setRole] = React.useState("mentor");
   const [isLoading, setIsLoading] = React.useState(false);
-
-  const handleToggleRole = () => {
-    if (role === "mentor") {
-      setRole("mentee");
-    } else {
-      setRole("mentor");
-    }
-  };
 
   const handleClick = async () => {
     setIsLoading(true);
-
-    if (role === "mentee") {
-      router.push("/pricing");
-    } else {
-      router.push("/dashboard/search");
-    }
+    router.push("/dashboard/search");
   };
 
   return (
     <div className="min-h-screen bg-white bg-[radial-gradient(60%_120%_at_50%_50%,hsla(0,0%,100%,0)_0,rgba(252,205,238,.5)_100%)]">
       <div className="form-container pt-10">
         <div className="card-block space-y-4">
-          {/* Heading */}
-          <div className="flex items-center justify-between">
-            <div className="flex items-center gap-4 justify-start">
-              <HeartHandshake className="w-10 h-10" />
-              <h3 className="h3">Code of conduct</h3>
-            </div>
-            <div className="flex items-center space-x-2">
-              <Label htmlFor="role">Mentor</Label>
-              <Switch id="role" onCheckedChange={handleToggleRole} />
-              <Label htmlFor="role">Mentee</Label>
-            </div>
+          <h3 className="h3 flex items-center gap-4 justify-start">
+            <HeartHandshake className="w-10 h-10" />
+            Code of conduct
+          </h3>
+          <p className="large">
+            A lot of things are obvious and won’t even write them here (like
+            respect). But here is a quick reminder of some things to keep in
+            mind:
+          </p>
+
+          <div className="p-6 rounded-md">
+            <ul className="space-y-3">
+              <ConductItem
+                text="Be attentive and engaged during sessions and aim to contribute
+          positively to the conversation."
+              />
+              <ConductItem
+                text="Add to the community’s good vibes by
+          being communicating effectively."
+              />
+              <ConductItem text="Time is precious. Be punctual and prepared for your sessions." />
+
+              <li className="flex items-center gap-4">
+                <CheckIcon className="h-5 w-5 shrink-0 text-primary-500" />
+                <span>
+                  Reflect our shared core{" "}
+                  <Link
+                    href="https://mentorscx.com/about#core-values"
+                    className="text-blue-500 hover:underline"
+                    target="_blank"
+                  >
+                    values
+                  </Link>{" "}
+                  on your behavior: community-focused, lifelong learner,
+                  humbleness, and own your growth.{" "}
+                </span>
+              </li>
+              <ConductItem
+                text="Just be a good human. Be kind, patient, and treat others the best
+          way you can."
+              />
+            </ul>
           </div>
-          {/* Body */}
-          {role === "mentee" && (
-            <div className="bg-secondary-100 p-6 rounded-md">
-              <div>
-                <ul className="space-y-3">
-                  <li className="flex gap-3">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke-width="1.5"
-                      stroke="currentColor"
-                      className="h-5 w-5  shrink-0 text-secondary-500"
-                    >
-                      <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        d="M4.5 12.75l6 6 9-13.5"
-                      />
-                    </svg>
-                    Share team inboxes
-                  </li>
-                  <li className="flex gap-3">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke-width="1.5"
-                      stroke="currentColor"
-                      className="h-5 w-5  shrink-0 text-secondary-500"
-                    >
-                      <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        d="M4.5 12.75l6 6 9-13.5"
-                      />
-                    </svg>
-                    Deliver instant answers
-                  </li>
-                  <li className="flex gap-3">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke-width="1.5"
-                      stroke="currentColor"
-                      className="h-5 w-5  shrink-0 text-secondary-500"
-                    >
-                      <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        d="M4.5 12.75l6 6 9-13.5"
-                      />
-                    </svg>
-                    Manage your team with reports
-                  </li>
-                  <li className="flex gap-3">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke-width="1.5"
-                      stroke="currentColor"
-                      className="h-5 w-5  shrink-0 text-secondary-500"
-                    >
-                      <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        d="M4.5 12.75l6 6 9-13.5"
-                      />
-                    </svg>
-                    Connect with customers
-                  </li>
-                  <li className="flex gap-3">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke-width="1.5"
-                      stroke="currentColor"
-                      className="h-5 w-5  shrink-0 text-secondary-500"
-                    >
-                      <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        d="M4.5 12.75l6 6 9-13.5"
-                      />
-                    </svg>
-                    Connect the tools you already use
-                  </li>
-                </ul>
-              </div>
-            </div>
-          )}
-
-          {role === "mentor" && (
-            <div className="bg-primary-100 p-6 rounded-md">
-              <div>
-                <ul className="space-y-3">
-                  <li className="flex gap-3">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke-width="1.5"
-                      stroke="currentColor"
-                      className="h-5 w-5  shrink-0 text-primary-500"
-                    >
-                      <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        d="M4.5 12.75l6 6 9-13.5"
-                      />
-                    </svg>
-                    Share team inboxes
-                  </li>
-                  <li className="flex gap-3">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke-width="1.5"
-                      stroke="currentColor"
-                      className="h-5 w-5  shrink-0 text-primary-500"
-                    >
-                      <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        d="M4.5 12.75l6 6 9-13.5"
-                      />
-                    </svg>
-                    Deliver instant answers
-                  </li>
-                  <li className="flex gap-3">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke-width="1.5"
-                      stroke="currentColor"
-                      className="h-5 w-5  shrink-0 text-primary-500"
-                    >
-                      <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        d="M4.5 12.75l6 6 9-13.5"
-                      />
-                    </svg>
-                    Manage your team with reports
-                  </li>
-                  <li className="flex gap-3">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke-width="1.5"
-                      stroke="currentColor"
-                      className="h-5 w-5  shrink-0 text-primary-500"
-                    >
-                      <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        d="M4.5 12.75l6 6 9-13.5"
-                      />
-                    </svg>
-                    Connect with customers
-                  </li>
-                  <li className="flex gap-3">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke-width="1.5"
-                      stroke="currentColor"
-                      className="h-5 w-5  shrink-0 text-primary-500"
-                    >
-                      <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        d="M4.5 12.75l6 6 9-13.5"
-                      />
-                    </svg>
-                    Connect the tools you already use to the mentors
-                  </li>
-                </ul>
-              </div>
-            </div>
-          )}
-
-          <div className="flex justify-between">
-            <Link href="/onboarding/5" className="muted underline">
+          <div className="flex justify-between ">
+            <Link
+              href="/onboarding/5"
+              target="_blank"
+              className="muted underline invisible"
+            >
               Learn more about the code of conduct
             </Link>
             <Button
@@ -248,14 +85,12 @@ const Onboard5Page = () => {
               onClick={handleClick}
               className="min-w-[100px] rounded-full"
             >
-              <span>
-                {isLoading ? (
-                  <Loader2Icon className="animate-spin h-4 w-4 mr-1" />
-                ) : (
-                  <CheckIcon className="h-4 w-4 mr-1" />
-                )}
-              </span>
-              <span>Yes, I agree!</span>
+              {isLoading ? (
+                <Loader2Icon className="animate-spin h-4 w-4 mr-1" />
+              ) : (
+                <CheckIcon className="h-4 w-4 mr-1" />
+              )}
+              Yes, I agree!
             </Button>
           </div>
         </div>
