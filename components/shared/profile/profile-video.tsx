b1283f08c6d0192767639ea3dee5193ba9025e5b
mentorscx <mentorscx@gmail.com>
2024-06-24 02:35:14 -0400

    changed video schema and size

--- components/shared/profile/profile-video.tsx
+++ components/shared/profile/profile-video.tsx
@@ -3,14 +3,16 @@ import { Card } from "@/components/ui/card";
 import React from "react";
 
 interface YouTubeVideoProps {
-  videoURL: string;
+  videoURL: string | null;
 }
 
 const YouTubeVideo = ({ videoURL }: YouTubeVideoProps) => {
+  if (videoURL === null) return null;
+
   return (
     <Card className="max-w-4xl mx-auto mt-4 rounded-full">
       <iframe
-        className="w-full aspect-video min-h-[200px] md:h-[400px] rounded-lg"
+        className="w-full aspect-video min-h-[250px] md:h-[480px] rounded-lg"
         src={videoURL}
         title="YouTube video"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
