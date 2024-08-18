b317f32a9c22f7ed1dcc0b1e14da53595e861f34
mentorscx <mentorscx@gmail.com>
2024-08-18 11:54:53 -0500

    stripe: corrected stripe success URL

--- app/api/webhook/stripe/route.ts
+++ app/api/webhook/stripe/route.ts
@@ -21,6 +21,7 @@ export async function POST(req: Request) {
       process.env.STRIPE_WEBHOOK_SECRET as string
     );
   } catch (error: unknown) {
+    console.error(error);
     return new Response("webhook error", { status: 400 });
   }
 
