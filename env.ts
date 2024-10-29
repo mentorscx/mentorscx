eb4cd01231bf8f66e6af4835dcbd36868cf1e242
mentorscx <mentorscx@gmail.com>
2024-10-29 12:09:09 +0530

    corrected GTM

--- env.ts
+++ env.ts
@@ -9,7 +9,6 @@ export const env = createEnv({
     STREAM_SECRET: z.string().min(1),
     CLERK_SECRET_KEY: z.string().min(1),
     BREVO_API_KEY: z.string().min(1),
-    GOOGLE_ANALYTICS_KEY: z.string().min(1),
   },
   client: {
     NEXT_PUBLIC_STREAM_KEY: z.string().min(1),
@@ -25,12 +24,13 @@ export const env = createEnv({
     // Stripe Price IDs for Sun Plan
     NEXT_PUBLIC_STRIPE_SUN_MONTHLY_PRICE_ID: stripePriceIdSchema,
     NEXT_PUBLIC_STRIPE_SUN_ANNUAL_PRICE_ID: stripePriceIdSchema,
+    NEXT_PUBLIC_GTM_ID: z.string().min(1),
   },
   runtimeEnv: {
     BREVO_API_KEY: process.env.BREVO_API_KEY,
     STREAM_SECRET: process.env.STREAM_SECRET,
     CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
-    GOOGLE_ANALYTICS_KEY: process.env.GOOGLE_ANALYTICS_KEY,
+    NEXT_PUBLIC_GTM_ID: process.env.NEXT_PUBLIC_GTM_ID,
     NEXT_PUBLIC_STREAM_KEY: process.env.NEXT_PUBLIC_STREAM_KEY,
     NEXT_PUBLIC_WEBSITE_URL: process.env.NEXT_PUBLIC_WEBSITE_URL,
     NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY:
